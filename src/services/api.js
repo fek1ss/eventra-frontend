36bd30f75dc5af5ccb352f5440740baf00a53cee
fek1ss <140281721+avantuurist@users.noreply.github.com>
2025-11-27 14:57:58 +0600

    feat: update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://eventra-backend-production.up.railway.app/'; // или URL твоего бекенда на Railway
+export const BASE_URL = 'https://eventra-backend-production.up.railway.app'; // или URL твоего бекенда на Railway
 
 export const getAuthHeaders = () => {
   const token = localStorage.getItem('token'); // хранение JWT после логина
