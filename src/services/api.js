d2052e591e4df653472e02af4d3c8e546e73345d
fek1ss <140281721+avantuurist@users.noreply.github.com>
2025-11-27 17:21:57 +0600

    base url, cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
-export const BASE_URL = 'http://localhost:5000'; // или URL твоего бекенда на Railway
+export const BASE_URL = 'https://eventra-backend-production.up.railway.app'; // или URL твоего бекенда на Railway
 // https://eventra-backend-production.up.railway.app
+// http://localhost:5000
 export const getAuthHeaders = () => {
   const token = localStorage.getItem('token'); // хранение JWT после логина
   return {
