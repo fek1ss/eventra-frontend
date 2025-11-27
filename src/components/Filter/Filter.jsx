096937ff96e9b1404dab3b095446d78f1ce237b8
fek1ss <140281721+avantuurist@users.noreply.github.com>
2025-11-27 19:49:47 +0600

    feat: search engine

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,32 @@
+const Filter = () => {
+  return (
+    <select name="">
+      <option
+        value="
+      "
+      >
+        Ярмарка
+      </option>
+      <option
+        value="
+      "
+      >
+        Концерт
+      </option>
+      <option
+        value="
+      "
+      >
+        Музыка
+      </option>
+      <option
+        value="
+      "
+      >
+        Мероприятие
+      </option>
+    </select>
+  );
+};
+
+export default Filter;
