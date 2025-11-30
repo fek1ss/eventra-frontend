ea474d67e96093b1bc7bd96080f5f8b390eea458
fek1ss <140281721+avantuurist@users.noreply.github.com>
2025-11-30 18:14:44 +0600

    feat: я устал

--- src/pages/AdminPages/AdminEvent/AdminEvent.jsx
+++ src/pages/AdminPages/AdminEvent/AdminEvent.jsx
@@ -21,6 +21,7 @@ const AdminEvent = () => {
     location: '',
     image: null,
     price: '',
+    responsible_phone: '',
   });
   useEffect(() => {
     if (state) {
@@ -34,6 +35,7 @@ const AdminEvent = () => {
           category: data.category,
           location: data.location,
           price: data.price,
+          responsible_phone: data.responsible_phone,
         });
       });
     }
@@ -62,7 +64,8 @@ const AdminEvent = () => {
       !event.time ||
       !event.organization ||
       !event.category ||
-      !event.location
+      !event.location ||
+      !event.responsible_phone
     ) {
       showMessage('fill in all fields', true);
       return;
@@ -82,6 +85,7 @@ const AdminEvent = () => {
           location: '',
           image: null,
           price: '',
+          responsible_phone: '',
         });
       }
     } catch (err) {
@@ -214,21 +218,39 @@ const AdminEvent = () => {
           />
         </label>
       </div>
-      <label htmlFor="location" className={styles.label__price}>
-        Цена: (Не обязательно)
-        <input
-          className={styles.inp}
-          name="price"
-          type="text"
-          value={event.price || ''}
-          onChange={val =>
-            setEvent(prev => ({
-              ...prev,
-              price: val.target.value,
-            }))
-          }
-        />
-      </label>
+
+      <div className={styles.row}>
+        <label htmlFor="location" className={styles.label}>
+          Цена: (Не обязательно)
+          <input
+            className={styles.inp}
+            name="price"
+            type="text"
+            value={event.price || ''}
+            onChange={val =>
+              setEvent(prev => ({
+                ...prev,
+                price: val.target.value,
+              }))
+            }
+          />
+        </label>
+        <label htmlFor="responsible_phone" className={styles.label}>
+          Номер ответственного:
+          <input
+            className={styles.inp}
+            name="responsible_phone"
+            type="text"
+            value={event.responsible_phone}
+            onChange={val =>
+              setEvent(prev => ({
+                ...prev,
+                responsible_phone: val.target.value,
+              }))
+            }
+          />
+        </label>
+      </div>
 
       {state ? (
         <button
